47f7e698b88f2b1916700ad4cbba83993b2b9093
Jisung Im <j4im@edu.uwaterloo.ca>
2019-09-20 04:20:21 -0400

    added install file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cp -f bashrc ~/.bashrc 
 cp -f zshrc ~/.zshrc
 cp -f vimrc ~/.vimrc 
-cp -f agnosterof.zsh-theme ~/.oh-my-zsh/theme/agnosterof.zsh-theme
+cp -f agnosterof.zsh-theme ~/.oh-my-zsh/themes/agnosterof.zsh-theme
